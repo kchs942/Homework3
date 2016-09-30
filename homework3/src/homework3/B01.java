fd0bb8f70dd370b3c99a4f09f75593aeb26f7e2c
kim chang sik <kchs942@naver.com>
2016-09-30 18:44:04 +0900

    1번문제 완료

--- homework3/src/homework3/B01.java
+++ homework3/src/homework3/B01.java
@@ -1,10 +1,52 @@
 package homework3;
+import java.util.*;
 
-public class B01 {
+class B01{
+            int num1;
+            int num2;
+            int num3;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-     System.out.print("hello");
-	}
+           void input(){
+                   Scanner s=new Scanner(System.in);
+                 System.out.print("첫번쨰 숫자를 입력하세요");
+                 this.num1=s.nextInt();
+                 System.out.print("두번쨰 숫자를 입력하세요");
+                 this.num2=s.nextInt();
+                 System.out.print("세번쨰 숫자를 입력하세요");
+                 this.num3=s.nextInt();
 
+                print();
+
+           }
+
+           void print(){
+                 int max_num;
+                 int min_num;
+
+                 if(this.num1<this.num2){
+                   max_num=this.num2;
+                   min_num=this.num1;
+
+                 }
+                 else {
+                     max_num=this.num1;
+                      min_num=this.num2;
+                 }
+
+                 if(max_num<this.num3)
+                       max_num=this.num3;
+                 if(min_num>this.num3)
+                       min_num=this.num3;
+
+
+                   System.out.print("가장큰수는"+max_num+"가장작은수는"+min_num);
+
+           }
+           public static void main(String[] args){
+
+               new B01().input();
+
+               }
 }
+
+
